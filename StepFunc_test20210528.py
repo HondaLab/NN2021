890cd03375f8d0ab789089b2bded552e800dad53
18024005 <18024005@mmm.muroran-it.ac.jp>
2021-05-28 17:15:05 +0900

    コミットのテストです。

---
+++ StepFunc_test20210528.py
@@ -0,0 +1,16 @@
+
+#pushのテストです。
+import numpy as np
+import matplotlib.pyplot as plt
+
+def step_function(x):
+    return np.where(x<=0, 0, 1)
+    
+x=np.linspace(-5,5)
+y=step_function(x)
+
+print(x)
+print(y)
+
+plt.plot(x,y)
+plt.show()
